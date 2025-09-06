e16d06c07b906d8dd376a6c1d977b080ddcca43a
John Ford <johnthejolly@gmail.com>
2025-09-06 04:55:19 -0500

    Update kali-setup.sh

--- kali/kali-setup.sh
+++ kali/kali-setup.sh
@@ -97,7 +97,7 @@ sudo systemctl restart rsyslog
 # Prepare template for cloning
 sudo truncate -s 0 /etc/machine-id
 sudo rm /var/lib/dbus/etc-machineid
-sudo ln -s /etc/machine-id /var/lib/dbus/etc-machineid
+sudo ln -s /etc/machine-id /var/lib/dbus/machineid
 sudo rm /etc/ssh/ssh_host_*
 sudo systemctl enable cloud-init
 
